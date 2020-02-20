902b6e770550c6daf42b93b29dd14dda8e338633
smilen <smilenlyubenov@gmail.com>
2020-02-20 15:33:24 +0200

    Basic HTML

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ function App() {
                   <span
                     className="sr-only"
                   >
-                      (current)
+                    (current)
                   </span>
                 </a>
               </li>
@@ -60,7 +60,6 @@ function App() {
                 <h5 className="card-title">Ferrari F40</h5>
                 <p className="card-text">A</p>
                 <p className="card-text">100$</p>
-                <p className="card-text" />
                 <button className="card-btn" type="button" className="btn btn-dark">Dark</button>
               </div>
             </div>
@@ -70,14 +69,14 @@ function App() {
               <img
                 className="card-img"
                 src="https://www.internationalcollectables.com/blobs/Images/Cars/16/d9c4ca99-5150-491e-b0bb-76756f701d35.jpg?width=2000&height=1333"
-                alt="Card image"
+                alt="car"
               />
               <div className="card-img-overlay">
                 <h5 className="card-title">Ferrari F40</h5>
                 <p className="card-text">A</p>
                 <p className="card-text">100$</p>
                 <p className="card-text" />
-                <button className="card-btn" type="button" className="btn btn-dark">Dark</button>
+                <button type="button" className=" card-btn btn btn-dark">Dark</button>
               </div>
             </div>
           </div>
@@ -164,6 +163,101 @@ function App() {
             </tbody>
           </table>
         </div>
+      </div>
+      <div className="container">
+        <duv className="row">
+          <div className="col-4">
+            <div className="card bg-dark text-white">
+              <img
+                className="card-img"
+                src="https://www.internationalcollectables.com/blobs/Images/Cars/16/d9c4ca99-5150-491e-b0bb-76756f701d35.jpg?width=2000&height=1333"
+                alt="Card image"
+              />
+              <div className="card-img-overlay">
+                <h5 className="card-title">Ferrari F40</h5>
+                <p className="card-text">A</p>
+                <p className="card-text">100$</p>
+                <button className="card-btn" type="button" className="btn btn-dark">Dark</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="form-row">
+              <div className="col-md-12 mb-3">
+                <label htmlFor="validationServer01">First name</label>
+                <input
+                  type="text"
+                  className="form-control is-valid"
+                  id="validationServer01"
+                  placeholder="First name"
+                  value="Mark"
+                  required
+                />
+                <div className="valid-feedback">
+                  Looks good!
+                </div>
+              </div>
+              <div className="col-md-12 mb-3">
+                <label htmlFor="validationServer02">Last name</label>
+                <input
+                  type="text"
+                  className="form-control is-valid"
+                  id="validationServer02"
+                  placeholder="Last name"
+                  value="Otto"
+                  required
+                />
+                <div className="valid-feedback">
+                  Looks good!
+                </div>
+              </div>
+              <div className="col-md-12 mb-3">
+                <label htmlFor="validationServer02">Age</label>
+                <input
+                  type="text"
+                  className="form-control is-valid"
+                  id="validationServer02"
+                  placeholder="Last name"
+                  value="Otto"
+                  required
+                />
+                <div className="valid-feedback">
+                  Looks good!
+                </div>
+              </div>
+              <div className="col-md-12 mb-3">
+                <label htmlFor="validationServer02">Date</label>
+                <input
+                  type="text"
+                  className="form-control is-valid"
+                  id="validationServer02"
+                  placeholder="Last name"
+                  value="Otto"
+                  required
+                />
+                <div className="valid-feedback">
+                  Looks good!
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <h2>Estimated price</h2>
+            <table className="table table-striped table-dark">
+              <tbody>
+                <tr>
+                  <td>Mark</td>
+                  <td>Otto</td>
+
+                </tr>
+                <tr>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </duv>
       </div>
     </div>
   );

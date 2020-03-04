ac3148c49e04532d9a4c59ddf6053e20fae67510
smilen <smilenlyubenov@gmail.com>
2020-03-04 13:31:39 +0200

    Search basic

--- client/src/components/cars/cars.jsx
+++ client/src/components/cars/cars.jsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getCars } from '../../redux';
 import Car from '../car/car';
 
 const Cars = ({ cars, storageCars }) => {
+  const [search, setSearch] = useState();
+  const [q, setQ] = useState();
   useEffect(() => {
     storageCars();
   }, []);
+
+  const carsArray = cars.allCars.data;
+
+  const searchCar = (car) => carsArray
+    .filter((item) => item.model.toLowerCase().includes(car.toLowerCase()));
 
   return cars.loading ? (
     <h2>Loading</h2>
@@ -16,12 +23,51 @@ const Cars = ({ cars, storageCars }) => {
   ) : (
     <div className="container mt-4">
       <div className="row">
-        {cars
-                && cars.allCars.data
-                && cars.allCars.data.map((item) => (
-                  <Car key={item.id} car={item} />
-                ))}
+        <div className="active-cyan-3 active-cyan-4 mb-4">
+          <input
+            className="form-control mb-2"
+            type="text"
+            value={q}
+            placeholder="Search"
+            aria-label="Search"
+            onChange={(e) => setQ(e.target.value)}
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+            onClick={() => setSearch(searchCar(q))}
+          >
+            Search
+          </button>
+        </div>
       </div>
+      {
+        search
+        && (
+        <div className="row">
+          <h1>Search result</h1>
+          <div className="row">
+            {
+              search.map((item) => (
+                <Car key={item.id} car={item} />
+              ))
+            }
+          </div>
+        </div>
+        )
+      }
+      <hr />
+      <div className="row">
+        <h1>All cars</h1>
+        <div className="row">
+          {cars
+          && cars.allCars.data
+          && carsArray.map((item) => (
+            <Car key={item.id} car={item} />
+          ))}
+        </div>
+      </div>
+
     </div>
   );
 };
--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -25,15 +25,6 @@ const Navbar = () => (
             <NavLink activeClassName="active" className="nav-link" to="/cars">Cars</NavLink>
           </li>
         </ul>
-        <form className="form-inline mt-2 mt-md-0">
-          <input
-            className="form-control mr-sm-2"
-            type="text"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>

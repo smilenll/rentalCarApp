4ba075eb24acad33eb08154e9df97c1828297945
smilen <smilen.lyubenov@atscale.com>
2020-03-18 19:22:18 +0200

    Search component works with sum bugs and strong capling to cars

--- client/src/components/cars/cars.jsx
+++ client/src/components/cars/cars.jsx
@@ -3,18 +3,14 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getCars } from '../../redux';
 import Car from '../car/car';
+import Search from '../../shered/search/search';
 
 const Cars = ({ cars, storageCars }) => {
-  const [search, setSearch] = useState();
-  const [q, setQ] = useState();
   useEffect(() => {
     storageCars();
   }, []);
 
   const carsArray = cars.allCars.data;
-
-  const searchCar = (car) => carsArray
-    .filter((item) => item.model.toLowerCase().includes(car.toLowerCase()));
 
   return cars.loading ? (
     <h3>Loading</h3>
@@ -22,50 +18,7 @@ const Cars = ({ cars, storageCars }) => {
     <h3>{cars.error}</h3>
   ) : (
     <div className="container mt-4">
-      <div className="row">
-        <div className="col-3">
-          <input
-            className="form-control mb-2"
-            type="search"
-            value={q}
-            placeholder="Search"
-            aria-label="Search"
-            onChange={(e) => setQ(e.target.value)}
-          />
-        </div>
-        <div className="col-1">
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-            onClick={() => setSearch(searchCar(q))}
-          >
-            Search
-          </button>
-        </div>
-      </div>
-      {
-        search
-        && (
-          search.length > 0 ? (
-            <div className="row">
-              <h3>Search result</h3>
-              <div className="row">
-                {
-                    search.map((item) => (
-                      <Car key={item.id} car={item} />
-                    ))
-                  }
-              </div>
-            </div>
-          )
-            : (
-              <div className="row">
-                <h2 className="notFound">No result</h2>
-              </div>
-            )
-        )
-      }
-      <hr />
+      <Search items={carsArray} />
       <div className="row">
         <h3>All cars</h3>
         <div className="row">

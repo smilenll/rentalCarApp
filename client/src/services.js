6f5b883cfc86bbed9bfda3b819a33cefdac65cf7
smilen <smilen.lyubenov@atscale.com>
2020-03-27 14:29:54 +0200

    Animation on return car request

--- client/src/services.js
+++ client/src/services.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
 export const returnCar = async (contractId, returnDate) => {
-  axios.put(`http://localhost:4000/api/contracts/${contractId}`, returnDate)
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
+  let response;
+  await axios.put(`http://localhost:4000/api/contracts/${contractId}`, returnDate)
+    .then((contract) => response = contract.data)
+    .catch((error) => response = error);
+
+  return response;
 };
 
 export const singleCar = async (carId) => {
-  let car = {}
+  let car = {};
   await axios.get(`http://localhost:4000/api/cars/${carId}`)
     .then((response) => car = response.data)
     .catch((error) => console.log(error));

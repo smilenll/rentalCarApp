49b35c6d532abe2bba9c3c94f7285949e5ddbbdf
smilen <smilen.lyubenov@atscale.com>
2020-04-14 18:02:22 +0300

    Update db unique names #47

--- api/src/database/entities/model.entity.ts
+++ api/src/database/entities/model.entity.ts
@@ -14,7 +14,7 @@ export class Model {
     @PrimaryGeneratedColumn('increment')
     public id: number;
 
-    @Column('varchar', {length: 50})
+    @Column('varchar', {length: 50, unique: true})
     public name: string;
 
     @Column({type: 'boolean', default: false})
--- api/src/database/migration/1586875437729-V2DB.ts
+++ api/src/database/migration/1586875437729-V2DB.ts
@@ -1,15 +1,15 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class V2DB1586419369919 implements MigrationInterface {
-    name = 'V2DB1586419369919'
+export class V2DB1586875437729 implements MigrationInterface {
+    name = 'V2DB1586875437729'
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`ALTER TABLE "cars" DROP CONSTRAINT "FK_0d84b16e0ac645021378dfb170e"`, undefined);
-        await queryRunner.query(`CREATE TABLE "amortizations" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "from" integer NOT NULL DEFAULT 0, "to" integer NOT NULL DEFAULT 0, "isDeleted" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_d58cd112eb3ea59d6f8fe366ff9" PRIMARY KEY ("id"))`, undefined);
-        await queryRunner.query(`CREATE TABLE "manufacturers" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "isDeleted" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_138520de32c379a48e703441975" PRIMARY KEY ("id"))`, undefined);
-        await queryRunner.query(`CREATE TABLE "models" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "isDeleted" boolean NOT NULL DEFAULT false, "carClassId" integer, "manufactureId" integer, CONSTRAINT "PK_ef9ed7160ea69013636466bf2d5" PRIMARY KEY ("id"))`, undefined);
-        await queryRunner.query(`ALTER TABLE "cars" DROP COLUMN "carClassId"`, undefined);
+        await queryRunner.query(`CREATE TABLE "amortizations" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "from" integer NOT NULL DEFAULT 0, "to" integer NOT NULL DEFAULT 0, "isDeleted" boolean NOT NULL DEFAULT false, CONSTRAINT "UQ_45e68eb2bd89a0cd80859c05a7b" UNIQUE ("name"), CONSTRAINT "PK_d58cd112eb3ea59d6f8fe366ff9" PRIMARY KEY ("id"))`, undefined);
+        await queryRunner.query(`CREATE TABLE "manufacturers" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "isDeleted" boolean NOT NULL DEFAULT false, CONSTRAINT "UQ_379a6015606c983f18c0b03e9e2" UNIQUE ("name"), CONSTRAINT "PK_138520de32c379a48e703441975" PRIMARY KEY ("id"))`, undefined);
+        await queryRunner.query(`CREATE TABLE "models" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "isDeleted" boolean NOT NULL DEFAULT false, "carClassId" integer, "manufactureId" integer, CONSTRAINT "UQ_3492c71396207453cf17c0928fb" UNIQUE ("name"), CONSTRAINT "PK_ef9ed7160ea69013636466bf2d5" PRIMARY KEY ("id"))`, undefined);
         await queryRunner.query(`ALTER TABLE "cars" DROP COLUMN "model"`, undefined);
+        await queryRunner.query(`ALTER TABLE "cars" DROP COLUMN "carClassId"`, undefined);
         await queryRunner.query(`ALTER TABLE "cars" ADD "yearOfManufacture" integer`, undefined);
         await queryRunner.query(`ALTER TABLE "cars" ADD "modelId" integer`, undefined);
         await queryRunner.query(`ALTER TABLE "contracts" ALTER COLUMN "returnDateTime" SET DEFAULT null`, undefined);
@@ -25,8 +25,8 @@ export class V2DB1586419369919 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "contracts" ALTER COLUMN "returnDateTime" DROP DEFAULT`, undefined);
         await queryRunner.query(`ALTER TABLE "cars" DROP COLUMN "modelId"`, undefined);
         await queryRunner.query(`ALTER TABLE "cars" DROP COLUMN "yearOfManufacture"`, undefined);
-        await queryRunner.query(`ALTER TABLE "cars" ADD "model" character varying(200) NOT NULL`, undefined);
         await queryRunner.query(`ALTER TABLE "cars" ADD "carClassId" integer`, undefined);
+        await queryRunner.query(`ALTER TABLE "cars" ADD "model" character varying(200) NOT NULL`, undefined);
         await queryRunner.query(`DROP TABLE "models"`, undefined);
         await queryRunner.query(`DROP TABLE "manufacturers"`, undefined);
         await queryRunner.query(`DROP TABLE "amortizations"`, undefined);

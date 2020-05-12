3c9dff0b4d2c147b960c0d723c801be03f2fa597
Todor Paskalev <todor.paskalev@gmail.com>
2020-05-12 14:43:29 +0300

    merge

--- client/src/services.js
+++ client/src/services.js
@@ -4,5 +4,12 @@ import { API_DOMAIN_NAME } from './configs/configs';
 export const returnCar = (contractId, returnDate) => axios.put(`${API_DOMAIN_NAME}/contracts/${contractId}`, returnDate)
   .then((contract) => contract.data);
 
-export const singleCar = (carId) => axios.get(`${API_DOMAIN_NAME}/cars/${carId}`)
-  .then((response) => response.data);
+export const singleCar = (carId) => {
+  axios.get(`http://localhost:4000/api/cars/${carId}`)
+    .then((response) => response.data ,(err) => {
+      console.log("err", err);
+      throw Error("parser errror")
+    }).catch((err) => {
+      console.log("err2", err);
+    });
+};
